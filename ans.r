17c27a7a8ac1c17d98caeec22f174543af14b0bd
jahnvitandon <tandon.jahnvi@gmail.com>
2018-11-25 13:57:19 +0530

    Create ans.r

---
+++ ans.r
@@ -0,0 +1,12 @@
+
+
+#Problem
+#1. Perform the below given activities:
+#a. Take a sample data set of your choice
+#b. Apply random forest, logistic regression using Spark R
+#c. Predict for new dataset
+
+
+#Answers
+#NOTE
+#topic not teach until now,wait until it was teach n then i will finish it
